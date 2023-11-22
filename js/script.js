b20dcbb7b9cb2e6f7b0d4974a333c7407455f36f
Gabriel Redouin-Innecco <dragonixgold@gmail.com>
2023-11-22 14:02:27 +0100

    changed navBar links to js events

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-const infoButton = document.querySelector('#infoNav');
+window.onload =
+    function() {
+  var indexNavButton = document.getElementById('indexNav');
+  if(indexNavButton != null) indexNavButton.onclick = gotoIndex;
 
-infoButton.addEventListener('click', function() {
-  console.log('Div was clicked!');
-});
+  var infoNavButton = document.getElementById('infoNav');
+  if(infoNavButton != null) infoNavButton.onclick = gotoInfo;
+
+  var materialNavButton = document.getElementById('matNav');
+  if(materialNavButton != null) materialNavButton.onclick = gotoMaterial;
+
+  var collabNavButton = document.getElementById('colabNav');
+  if(collabNavButton != null) collabNavButton.onclick = gotoCollab;
+}
+
+function gotoIndex() {
+  window.location.replace('index.html');
+}
+function gotoInfo() {
+  window.location.replace('informations.html');
+}
+function gotoMaterial() {
+  window.location.replace('material.html');
+}
+function gotoCollab() {
+  window.location.replace('collab.html');
+}
